f4b9993fc68c346b88262f8920598f49dbb3d09b
quarshie133 <wrightsprince@gmail.com>
2026-03-07 16:27:49 +0000

    refactoring

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,7 +13,7 @@ export async function Footer() {
   return (
     <footer className="border-t border-gray-200 dark:border-gray-800 mt-20 transition-colors duration-300">
       <div className="container mx-auto px-4 py-8 flex flex-col md:flex-row justify-between items-center text-sm text-gray-500 dark:text-gray-400">
-        <p>© {new Date().getFullYear()} Writer's Name. All rights reserved.</p>
+        <p>© {new Date().getFullYear()} Prince Henry. All rights reserved.</p>
         
         <div className="flex gap-6 mt-4 md:mt-0">
           {socials?.instagram && (
